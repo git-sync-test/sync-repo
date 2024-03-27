38392554efa0e7beec6478368b9e9f4b58f40fe8
vikash-paragon <vikash.kumar@useparagon.com>
2024-03-27 12:17:21 +0530

    fix yml path

---
+++ dist/integrations/slack/config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    description: "A description for the integration's purpose.",
+    overviewText: "A long-form description for the integration's purpose.",
+    showWatermark: true,
+};
+exports.default = config;
+//# sourceMappingURL=config.js.map
